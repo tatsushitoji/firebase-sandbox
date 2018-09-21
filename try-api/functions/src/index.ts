bfabc0eb106d6a92de4cd59a7ecaa7864bf68c8f
tatsushitoji <tatsushi.toji@gmail.com>
2018-09-21 17:51:29 +0900

    fix: tslint --fix

--- try-api/functions/src/index.ts
+++ try-api/functions/src/index.ts
@@ -4,15 +4,22 @@ import axios from 'axios';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest(async (request, response) => {
-    const {ghUser} = request.query;
-    await axios.get(`https://api.github.com/users/`+ ghUser +`/starred`)
-    .then(function (res) {
+
+const githubAPI = 'https://api.github.com/users/';
+const starred = '/starred';
+
+export const helloWorld = functions.https.onRequest(
+  async (request, response) => {
+    const { ghUser } = request.query;
+    await axios
+      .get(githubAPI + ghUser + starred)
+      .then(res => {
         response.status(200).json({ myRepos: res.data[1] });
-    })
-    .catch(function (error) {
+      })
+      .catch(error => {
         console.log(error.response);
-        const {status, statusText} = error.response;
+        const { status, statusText } = error.response;
         response.status(status).json(statusText);
-    });
-});
+      });
+  },
+);
